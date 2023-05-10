0673718889794686b30af0405e0d7584ef237a9f
chorongbali <chorongbali@gmail.com>
2023-05-10 16:25:01 +0800

    de

---
+++ Udemy/Begin DevOps Career As an Absolute Beginner | Linux, AWS, Scripting, Jenkins, Ansible, Docker, K8s, N-Tier Projects/linus/basics.sh
@@ -0,0 +1,54 @@
+
+### 1
+clear
+whoami
+exit
+clear
+whoami
+pwd
+ls
+cat /etc/os-release
+sudo -i
+
+
+### 2
+whoami
+pwd
+ls
+clear
+cd /
+pwd
+ls
+pwd
+cd /bin
+ls
+clear
+cd  /sbin/
+ls
+clear
+cd /etc/
+ls
+cat /etc/hostname
+clear
+cd /tmp/
+ls
+cd /boot/
+ls
+cd grub
+ls
+cd ..
+cd grub2/
+ls
+clear
+pwd
+cd
+pwd
+cd /proc/
+ls
+uptime
+cat /proc/uptime
+free -m
+clear
+ls
+cd
+clear

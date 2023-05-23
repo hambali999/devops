02f22c8962eb76196ebdf06d4e10c893c26b4dd9
chorongbali <chorongbali@gmail.com>
2023-05-24 00:51:58 +0800

    updates

--- Udemy/Begin DevOps Career As an Absolute Beginner | Linux, AWS, Scripting, Jenkins, Ansible, Docker, K8s, N-Tier Projects/Docker/DockerFiles/Exercises/ex2DockerVoumes.sh
+++ Udemy/Begin DevOps Career As an Absolute Beginner | Linux, AWS, Scripting, Jenkins, Ansible, Docker, K8s, N-Tier Projects/Docker/DockerFiles/Exercises/ex2DockerVoumes.sh
@@ -9,4 +9,8 @@
 
 #inspect images -> docker inspect mysql:latest
 
+# -v = tag for volume
+
+# docker run --name vprodb -d -e MYSQL_ROOT_PASSWORDD=secretpass -p 3030:3306 -v 
+
 docker inspect mysql:latest

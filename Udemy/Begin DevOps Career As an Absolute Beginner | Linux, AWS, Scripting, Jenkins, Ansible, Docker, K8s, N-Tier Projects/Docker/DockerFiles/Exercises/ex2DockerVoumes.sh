cf5e8ff4387013f9eb40e5fd1906634919cf4968
chorongbali <chorongbali@gmail.com>
2023-05-24 00:41:24 +0800

    updates

---
+++ Udemy/Begin DevOps Career As an Absolute Beginner | Linux, AWS, Scripting, Jenkins, Ansible, Docker, K8s, N-Tier Projects/Docker/DockerFiles/Exercises/ex2DockerVoumes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Container Data
+#   - Volumes
+#       - Managed by Docker (/var/lib/docker/volumes) on Linux
+#   - Bind Mounts
+#       - Stored anywhere on the host system
+
+
+#inspect images -> docker inspect mysql:latest
+
+docker inspect mysql:latest

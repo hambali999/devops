3d86694c1209d8387eceaba010c5cae8d04bda71
chorongbali <chorongbali@gmail.com>
2023-05-29 02:51:58 +0800

    updates

--- Udemy/Begin DevOps Career As an Absolute Beginner | Linux, AWS, Scripting, Jenkins, Ansible, Docker, K8s, N-Tier Projects/Docker/DockerFiles/Exercises/ex5DockerCompose/composetest/start.sh
+++ Udemy/Begin DevOps Career As an Absolute Beginner | Linux, AWS, Scripting, Jenkins, Ansible, Docker, K8s, N-Tier Projects/Docker/DockerFiles/Exercises/ex5DockerCompose/composetest/start.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 docker compose up
+
+
+
+# to stop/remove the docker containers running by compose
+
+docker compose down
+
+
+# run docker compose in detach mode
+
+docker compose up -d
